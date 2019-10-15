50b8bd020983424d45485e7856f627b6c21cc511
davillo <davidfrorup@gmail.com>
2019-10-15 08:48:35 +0200

    kage

--- AarhusBryghus/src/test/Test.java
+++ AarhusBryghus/src/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("hej med dig kagemand");
-		System.out.println("jeg kan lide katte");
+		System.out.println("jeg kan lide hunde");
 
 	}
 
