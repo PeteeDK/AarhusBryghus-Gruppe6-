3e794ff728c8eaa0472829bd2364d7f281cbe472
PeteeDk <peteerclausen@gmail.com>
2019-10-17 09:20:41 +0200

    Version 0.02

--- AarhusBryghus/src/kasseapperat/AnlægLeaf.java
+++ AarhusBryghus/src/kasseapperat/AnlægLeaf.java
@@ -1,5 +1,12 @@
 package kasseapperat;
 
 public class AnlægLeaf {
+	
+	private double pris; 
+	private boolean afleveret; 
+	private double brugtFustageMængde; 
+	private double brugtKulsyreMængde; 
+	
+	
 
 }
