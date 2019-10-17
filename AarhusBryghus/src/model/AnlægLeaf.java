1f9684face5261524874b8cb9a8db932e5ef660c
erikato-git <erik.kato@hotmail.com>
2019-10-17 13:49:17 +0200

    Auto stash before rebase of "origin/Erik"
    First push to own branch

---
+++ AarhusBryghus/src/model/AnlægLeaf.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class AnlægLeaf implements Produkt {
+	
+	private double pris; 
+	private boolean afleveret; 
+	private double brugtFustageMængde; 
+	private double brugtKulsyreMængde; 
+	
+	
+	public void fustageAfleveret() {
+		
+	}
+	
+	public void beregnPris() {
+		
+	}
+	
+
+}
