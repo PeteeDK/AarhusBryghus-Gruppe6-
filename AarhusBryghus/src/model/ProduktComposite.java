59eba6031ee19b8fe4e5e4ae986eecf0cb3c688e
PeteeDk <peteerclausen@gmail.com>
2019-10-21 11:52:35 +0200

    Revert "Revert "Merge branch 'master' into David""
    This reverts commit a72854c329ba00a2f678d4555f985ab135cba01e.

---
+++ AarhusBryghus/src/model/ProduktComposite.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class ProduktComposite extends Produkt {
+
+	private double samletPris; 
+	private LocalDate dato; 
+	private ArrayList<Produkt> produkt; 
+	
+	
+	public void beregnSamletPris() {
+		
+	}
+	
+	public void vælgBetalingsform() {
+		
+	}
+	
+	public void vælgSalgsSituation() {
+		
+	}
+	
+}
