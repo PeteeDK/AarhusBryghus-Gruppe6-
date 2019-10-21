c1afe45e5c10d1268a963c7fe5cfecf4d66bb689
PeteeDk <peteerclausen@gmail.com>
2019-10-21 11:52:43 +0200

    Revert "Merge branch 'master' into David"
    This reverts commit 46bd42a7b8cc6ddae1a365e3033816504b16edb9, reversing
    changes made to 966a98dc2dfeeaa2385abe8f9bba8a6a17ec386a.

---
+++ AarhusBryghus/src/testDavid/Test.java
@@ -0,0 +1,11 @@
+package testDavid;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hej");
+System.out.println("Hvorfor kat, når der er hund");
+	}
+
+}
