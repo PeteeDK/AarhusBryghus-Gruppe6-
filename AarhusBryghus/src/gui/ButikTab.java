92c9d1e39e9fbe9700567d31321817bd4842ec25
PeteeDk <peteerclausen@gmail.com>
2019-10-29 13:20:52 +0100

    Revert "slet mig"
    This reverts commit 578ea9f150f166f31f0c2a095b23bda7bb1509ca.

--- AarhusBryghus/src/gui/ButikTab.java
+++ AarhusBryghus/src/gui/ButikTab.java
@@ -9,7 +9,6 @@ import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import model.PrisListe;
 
 public class ButikTab {
 
@@ -20,14 +19,10 @@ public class ButikTab {
 	}
 	
 	public void open() {
-		
-		Controller.initStorage();
 	
 		Label lb1 = new Label("Produkter"); 
 		ListView<String> produkter = new ListView<>();
 		produkter.getItems().setAll(Controller.getButiksPrisliste());
-		
-		
 		TextField txf1 = new TextField();
 		txf1.setPromptText("Antal");
 		txf1.setEditable(true);
